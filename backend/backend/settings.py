2f7bc52befbb250d677450eeafa883ee9dfb63ea
vsdremlyuga <vsdremlyuga@cloud.ru>
2025-03-21 15:58:47 +0300

    1.2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ('django-insecure-j_89af+30&&4qm*8z9_('
               '^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['89.169.169.165', 'practicum250305.zapto.org']
 
 # Application definition
 
